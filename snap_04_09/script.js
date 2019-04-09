63e805b3eacce2c0a6d037220e4e26348ef05cc8
NickSchnepple <nschnepple@cnm.edu>
2019-04-09 08:48:06 -0600

    made function 1 true

--- snap_04_09/script.js
+++ snap_04_09/script.js
@@ -1,1 +1,10 @@
+function addNumbers($parameter1, $parameter2) {
+
+	$answer = $parameter1 + $parameter2;
+	return $answer
+}
+
+$myVariable = addNumbers(12, 5);
+
+console.log(addNumbers(12, 5));
 
