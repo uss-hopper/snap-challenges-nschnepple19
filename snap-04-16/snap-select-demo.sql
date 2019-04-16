5157a8d1e34a62f1e77091b29ece837fcdc5da3f
NickSchnepple <nschnepple@cnm.edu>
2019-04-16 14:43:10 -0600

    created solutions folder for 2nd part of snap

---
+++ snap-04-16/snap-select-demo.sql
@@ -0,0 +1,10 @@
+SELECT
+	tweet.tweetContent,
+		`like`.likeDate
+		profile.profileAtHandle
+FROM tweet
+		INNER JOIN `like` ON `like`.likeTweetId = tweet.tweetId
+INNER JOIN profile ON profile.profileId = `like`.likeProfileId
+	WHERE tweetId = HEX('0536faef082b454e9d444cdbe7887d7a');
+
+
--- snap-04-16/snap-task.sql
+++ snap-04-16/snap-task.sql
@@ -1,13 +1,14 @@
 drop table if exists task;
 
 create table task (
-taskId binary(20) not null,
-taskTitle varchar(255) not null,
-taskStartDate datetime null,
-taskDueDate datetime null,
-taskStatus varchar(64) not null,
-taskPriority varchar(64) not null,
-taskDescription varchar(256) null,
+	taskId binary(20) not null,
+	taskTitle varchar(255) not null,
+	taskStartDate datetime null,
+	taskDueDate datetime null,
+	taskStatus varchar(64) not null,
+	taskPriority varchar(64) not null,
+	taskDescription varchar(256) null,
 	index(taskId),
 	primary key(taskId)
 );
+
