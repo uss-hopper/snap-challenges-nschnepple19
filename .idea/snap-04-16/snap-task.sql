7916506f0d6ac463ffff95b6e9993c0e65fb83e3
NickSchnepple <nschnepple@cnm.edu>
2019-04-16 08:36:34 -0600

    fixed syntax with drop table

--- .idea/snap-04-16/snap-task.sql
+++ .idea/snap-04-16/snap-task.sql
@@ -1,11 +1,13 @@
+drop table if exists task;
+
 create table task (
 taskId binary(20) not null,
 taskTitle varchar(255) not null,
-taskStartDate datetime not null,
-taskDueDate datetime not null,
+taskStartDate datetime null,
+taskDueDate datetime null,
 taskStatus varchar(64) not null,
 taskPriority varchar(64) not null,
-taskDescription varchar(256) not null,
+taskDescription varchar(256) null,
 	index(taskId),
 	primary key(taskId)
 );
