a4b23d31a7646c7b52a0ab73edd3af2bab01b258
NickSchnepple <nschnepple@cnm.edu>
2019-04-18 08:55:02 -0600

    i failed miserably at the snap challenge

---
+++ snap-04-18/snap.js
@@ -0,0 +1,11 @@
+//given weightKg:
+//given weightLbs
+//set intermediaryVariable to weightKg 1;
+//set weightKg to intermediaryVariable * .454
+//return weightLbs
+
+
+function findPositiveFactors(positiveFactors) {
+	let factors = 12/2;
+	let
+}
